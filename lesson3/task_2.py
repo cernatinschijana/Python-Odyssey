2f912bbb1b70c913fde86550f0e4d385158c5516
cernatinschijana <cernatinschi.jana@gmail.com>
2024-04-08 20:45:53 +0300

    add homework for lesson3

---
+++ lesson3/task_2.py
@@ -0,0 +1,27 @@
+# Aceasta este a doua sarcină legată de booleeni în python
+# Creați o variabilă numită `is_student` și setați-o la `True`
+
+is_student = True
+
+# Acum tipăriți valoarea variabilei `is_student` folosind funcția `print`
+
+print(is_student)
+
+# Acum creați o nouă variabilă numită `is_child` și setați-o la `False` și tipăriți valoarea variabilei `is_child` folosind funcția `print`
+
+is_child = False
+print(is_child)
+
+# Acum utilizați operatorul `and` pentru a verifica dacă atât `is_student`, cât și `is_child` sunt `True` și tipăriți rezultatul folosind funcția `print`
+
+print(is_student and is_child)
+
+# Acum utilizați operatorul `or` pentru a verifica dacă cel puțin una dintre `is_student` și `is_child` este `True` și tipăriți rezultatul folosind funcția `print`
+
+print(is_student or is_child)
+
+# Acum utilizați operatorul `not` pentru a verifica dacă `is_student` este `False` și tipăriți rezultatul folosind funcția `print`
+
+print(not is_student == False)
+
+# Aceasta a fost tot pentru această sarcină
---
+++ lesson3/task_3.py
@@ -0,0 +1,62 @@
+# Aceasta este a treia ta sarcină legată de numere în python
+# Creează o variabilă numită `number` și atribuie-i valoarea `10`
+
+number = 10
+
+# Acum afișează valoarea variabilei `number` folosind funcția `print`
+
+print(number)
+
+# Acum creează o nouă variabilă numită `number2` și atribuie-i valoarea `number` și afișează valoarea variabilei `number2` folosind funcția `print`
+
+number2 = number
+print(number2)
+
+# Acum creează o nouă variabilă numită `number3` și atribuie-i valoarea `4`
+
+number3 = 4
+
+# Acum aplică toate operațiile pe care le-ai învățat în lecție asupra variabilelor `number` și `number3` și afișează rezultatele folosind funcția `print`
+# Operațiile sunt: adunare, scădere, înmulțire, împărțire, modulo, exponențiere, împărțire întreagă
+
+print(number + number3)
+print(number - number3)
+print(number * number3)
+print(number / number3)
+print(number % number3)
+print(number ** number3)
+print(number // number3)
+
+# Creează o nouă variabilă numită `number4` și atribuie-i o valoare numerică mare într-un mod literal
+
+number4 = 1_000_000
+
+# Acum afișează tipul variabilei `number4` folosind funcția `print`
+
+print(type(number4))
+
+# Creați o variabilă `numar` și atribuiții valoarea 5.0
+
+numar = 5.0
+
+# Acum afișați tipul lui `numar` folosind funcția `print`
+
+print(type(numar))
+
+# Acum creați o variabilă `numar2` și atribuiții valoarea 2.0
+
+numar2 = 2.0
+
+# Acum creați o variabila `numar_mare` și atribuiți-i o valoare mare utilizând numerele literale
+
+numar_mare = 4_667_789.0
+
+# Acum afișați tipul lui `numar_mare` folosind funcția `print`
+
+print(type(numar_mare))
+
+# Acum ștergeți variabila `numar_mare` folosind instrucțiunea `del`
+
+del numar_mare
+
+# Asta a fost tot pentru această sarcină
